e372c8a9efd851581e59ac78ef776a6bf5345a76
Mikael Deurell <mikael@deurell.net>
2023-01-13 11:09:40 +0100

    easy anchor implementation

--- SurfaceDetection/DetectionView.swift
+++ SurfaceDetection/DetectionView.swift
@@ -7,13 +7,13 @@
 
 import RealityKit
 import ARKit
+import Combine
 
 class DetectionView: ARView, ARSessionDelegate {
-    
-    /// Dictionary with id from the ARPlaneAnchor as key and a PlaneAnchorEntity as value
-    var planes = [UUID: PlaneAnchorEntity]()
-    
     var arView: ARView { return self }
+    
+    var anchorEntity: AnchorEntity?
+    var sub: Cancellable?
     
     required init(frame frameRect: CGRect) {
         super.init(frame: frameRect)
@@ -27,55 +27,34 @@ class DetectionView: ARView, ARSessionDelegate {
     func setup() {
         arView.cameraMode = .ar
         setupARSession()
+        setupScene()
     }
     
-    /// Start ARSession and setup self as delegate.
     private func setupARSession() {
         let session = self.session
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = [.horizontal]
         session.run(configuration)
-        session.delegate = self
     }
     
-    /// Anchors have been added. Add them to the dictionary and add PlaneAnchorEntities to the scene.
-    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
-        for anchor in anchors {
-            if let arPlaneAnchor = anchor as? ARPlaneAnchor {
-                let id = arPlaneAnchor.identifier
-                if planes.contains(where: {$0.key == id}) { fatalError("anchor already exists")}
-                let planeAnchorEntity = PlaneAnchorEntity(arPlaneAnchor: arPlaneAnchor)
-                self.scene.anchors.append(planeAnchorEntity)
-                planes[id] = planeAnchorEntity
-            }
+    private func setupScene() {
+        let anchor = AnchorEntity(.plane(.horizontal, classification: .any,
+                                         minimumBounds: [0.5, 0.5]))
+        printAnchorState()
+        
+        scene.anchors.append(anchor)
+        self.anchorEntity = anchor
+        
+        sub = arView.scene.subscribe(to: SceneEvents.AnchoredStateChanged.self) { event in
+            self.printAnchorState()
+            let mesh = MeshResource.generateBox(size: 0.1)
+            let material = SimpleMaterial(color: .blue, isMetallic: false)
+            let entity = ModelEntity(mesh: mesh, materials: [material])
+            self.anchorEntity?.addChild(entity)
         }
     }
     
-    /// Anchors have been updated. call didUpdate on the planeanchor.
-    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
-        for anchor in anchors {
-            if let planeAnchor = anchor as? ARPlaneAnchor {
-                if let planeEntityAnchor = planes[planeAnchor.identifier] {
-                    try? planeEntityAnchor.didUpdate(arPlaneAnchor: planeAnchor)
-                } else {
-                    fatalError("trying to update unexisting anchor")
-                }
-            }
-        }
-    }
-    
-    /// Anchors has been removed from the ARSession so remove them from our dictionary and also remove the planeanchors from the scene.
-    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
-        for anchor in anchors {
-            if let planeAnchor = anchor as? ARPlaneAnchor {
-                if let planeEntityAnchor = self.planes[planeAnchor.identifier] {
-                    self.planes.removeValue(forKey: planeAnchor.identifier)
-                    self.scene.anchors.remove(planeEntityAnchor)
-                } else {
-                    fatalError("trying to remove unexisting anchor")
-                }
-                
-            }
-        }
+    private func printAnchorState() {
+        print("isAnchored: \(self.anchorEntity?.isAnchored ?? false)")
     }
 }
