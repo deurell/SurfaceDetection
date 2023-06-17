4b0b7976275a1cbdaffb0c405f8607103c2124c0
Mikael Deurell <mikael@deurell.net>
2023-06-17 16:54:01 +0200

    better anchor update

---
+++ SurfaceDetection/simd_float4x4+Translation.swift
@@ -0,0 +1,20 @@
+//
+//  simd_float4x4+Translation.swift
+//  SurfaceDetection
+//
+//  Created by Mikael Deurell on 2023-06-17.
+//
+
+import RealityKit
+
+extension simd_float4x4 {
+    /// Provide conveniece init for float4x4 matrix with a translation.
+    init(translation: simd_float3) {
+        self.init(
+            simd_float4(1, 0, 0, translation.x),
+            simd_float4(0, 1, 0, translation.y),
+            simd_float4(0, 0, 1, translation.z),
+            simd_float4(0, 0, 0, 1)
+        )
+    }
+}
